1347656b6aad9722e4e9a6a539ffce01c847aa00
NidhiShivhareGit <nidhiec1062@gmail.com>
2022-02-20 22:22:21 +0530

    initial commit

---
+++ src/main/java/com/example/restservice/demo_resful/DemoResfulApplication.java
@@ -0,0 +1,14 @@
+package com.example.restservice.demo_resful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoResfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoResfulApplication.class, args);
+		System.out.println("Successful");
+	}
+
+}
